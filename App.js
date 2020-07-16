ec1c6b23f0c5eaac729815bc6c7384e15852d15a
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-07-16 22:51:02 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  createAppContainer,
+  createSwitchNavigator
+} from 'react-navigation'
+import {
+  createStackNavigator
+} from 'react-navigation-stack'
+import LoadingScreen from './screens/LoadingScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import * as firebase from 'firebase';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCDX7NCSvf3ux0qN6n1mnbZnNU6i7C8ouc",
+  authDomain: "socialapp-d5003.firebaseapp.com",
+  databaseURL: "https://socialapp-d5003.firebaseio.com",
+  projectId: "socialapp-d5003",
+  storageBucket: "socialapp-d5003.appspot.com",
+  messagingSenderId: "870003233000",
+  appId: "1:870003233000:web:6368479a6c4589ab99b95d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const AppStack = createStackNavigator({
+  Home: HomeScreen
+})
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen
+})
+
+export default createAppContainer(
+  createStackNavigator({
+    Loading: LoadingScreen,
+    App: AppStack,
+    Auth: AuthStack
+  }, {
+    initialRouteName: "Loading"
+  })
+)
